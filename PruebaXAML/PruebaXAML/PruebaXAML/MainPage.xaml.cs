bfe55ea6a008aa13490cef8879cf8a9bb8d47441
Estudiante <Estudiante@SALAPROF-2>
2019-04-01 23:33:33 -0500

    Practica basica hola juan

---
+++ PruebaXAML/PruebaXAML/PruebaXAML/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PruebaXAML
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
