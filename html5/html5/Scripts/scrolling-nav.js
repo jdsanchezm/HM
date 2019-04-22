a2646b479fb487a53df3f44ddb68806c814f5519
Juan David Sánchez Muñoz <juandasa0219@hotmail.com>
2019-04-22 17:05:31 -0500

    Modelo vc

---
+++ html5/html5/Scripts/scrolling-nav.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#app',
+    data: {
+        // Array con varios elementos.
+        elements: ['ele 1', 'ele 2', 'ele 3'],
+        users: [
+            { name: 'user 1', gender: 'm' },
+            { name: 'user 2', gender: 'f' },
+            { name: 'user 3', gender: 'f' }
+
+        ]
+    }
+})
+
