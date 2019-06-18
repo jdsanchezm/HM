ccd446f2919eb1bca767aee637df8e50aa26dde1
Estudiante <Estudiante@SALAPROF-2>
2019-06-17 22:58:45 -0500

    validacion

---
+++ FormularioWiliam/FormularioParte2/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txusuraio = New System.Windows.Forms.TextBox()
+        Me.txcontrasena = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btningresar = New System.Windows.Forms.Button()
+        Me.btncancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txusuraio
+        '
+        Me.txusuraio.Location = New System.Drawing.Point(388, 99)
+        Me.txusuraio.Name = "txusuraio"
+        Me.txusuraio.Size = New System.Drawing.Size(185, 20)
+        Me.txusuraio.TabIndex = 0
+        '
+        'txcontrasena
+        '
+        Me.txcontrasena.Location = New System.Drawing.Point(388, 154)
+        Me.txcontrasena.Name = "txcontrasena"
+        Me.txcontrasena.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txcontrasena.Size = New System.Drawing.Size(185, 20)
+        Me.txcontrasena.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(290, 102)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(290, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Contraseña"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(357, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Cinemas S.A."
+        '
+        'btningresar
+        '
+        Me.btningresar.Location = New System.Drawing.Point(48, 249)
+        Me.btningresar.Name = "btningresar"
+        Me.btningresar.Size = New System.Drawing.Size(705, 38)
+        Me.btningresar.TabIndex = 5
+        Me.btningresar.Text = "Ingresar"
+        Me.btningresar.UseVisualStyleBackColor = True
+        '
+        'btncancelar
+        '
+        Me.btncancelar.Location = New System.Drawing.Point(84, 294)
+        Me.btncancelar.Name = "btncancelar"
+        Me.btncancelar.Size = New System.Drawing.Size(640, 23)
+        Me.btncancelar.TabIndex = 6
+        Me.btncancelar.Text = "Cancelar"
+        Me.btncancelar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btncancelar)
+        Me.Controls.Add(Me.btningresar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txcontrasena)
+        Me.Controls.Add(Me.txusuraio)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txusuraio As TextBox
+    Friend WithEvents txcontrasena As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btningresar As Button
+    Friend WithEvents btncancelar As Button
+End Class
---
+++ FormularioWiliam/FormularioWiliam/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioWiliam
+{
+    public partial class Form1 : Form
+    {
+        private string usuario;
+        private string contrasena;
+        public string msjcorrecto = "La contraseña es correcta", msjincorrecto ="la contraseña es incorrecta";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ingresar_Click(object sender, EventArgs e)
+        {
+            usuario = "Juan";
+            contrasena = "1234";
+
+            if(txusuario.Text==usuario && txcontrasena.Text == contrasena)
+            {
+                c msjcorrecto;
+            }
+        }
+    }
+}
