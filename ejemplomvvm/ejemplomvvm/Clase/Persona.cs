98ebde72450906bcb90fe7f8b42f96ec83c08f22
Juan David Sánchez Muñoz <juandasa0219@hotmail.com>
2019-06-18 17:29:47 -0500

    mvvm

---
+++ ejemplomvvm/ejemplomvvm/Clase/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ejemplomvvm.Clase
+{
+    class Persona : INotifyPropertyChanged
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set
+            {
+                this.nombre = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Nombre"));
+            }
+        }
+        private int telefono;
+        public int Telefono
+        {
+            get { return this.telefono; }
+            set
+            {
+                this.telefono = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Telefono"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
