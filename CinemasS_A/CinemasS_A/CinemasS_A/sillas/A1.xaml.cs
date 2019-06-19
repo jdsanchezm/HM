f3f0f8200518f2332748bfe7f34bf410d274f279
Juan David Sánchez Muñoz <juandasa0219@hotmail.com>
2019-06-19 18:25:23 -0500

    proyecto

---
+++ CinemasS_A/CinemasS_A/CinemasS_A/sillas/A1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CinemasS_A.NewFolder
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class A1 : ContentPage
+	{
+		public A1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
